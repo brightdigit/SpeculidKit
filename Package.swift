3993485024b3bc743d5ce9e9fab6494883cf5b4b
Leo Dion <leogdion@brightdigit.com>
2020-04-06 20:13:11 -0400

    adding SwiftDraw

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
   name: "SpeculidKit",
+    platforms: [
+         .macOS(.v10_12), .iOS(.v10),
+      ],
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
     .library(
@@ -14,14 +17,17 @@ let package = Package(
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(path: "./packages/CairoSVG"),
+    
+    .package(path: "./packages/AssetLib"),
+    .package(path: "./packages/GampKit"),
+    .package(url: "https://github.com/swhitty/SwiftDraw.git", from: "0.7.0")
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .target(
       name: "SpeculidKit",
-      dependencies: []
+      dependencies: ["AssetLib", "GampKit", "SwiftDraw"]
     ),
     .testTarget(
       name: "SpeculidKitTests",
--- Sources/SpeculidKit/Speculid.swift
+++ Sources/SpeculidKit/Speculid.swift
@@ -1,4 +1,7 @@
 import Foundation
+import AssetLib
+import GampKit
+import SwiftDraw
 public struct FileTesting {
   public static func copyFile (from fromURL : URL, to toURL: URL) throws {
     try FileManager.default.copyItem(at: fromURL, to: toURL)
