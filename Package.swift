97b74c05e5d25bfdf47462d19a9818e7a9709345
Leo Dion <leogdion@brightdigit.com>
2020-06-23 14:39:52 -0400

    using xcframework

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,7 +12,7 @@ let package = Package(
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
     .library(
       name: "SpeculidKit",
-      targets: ["SpeculidKit"]
+      targets: ["SpeculidKit", "CairoSVG"]
     )
   ],
   dependencies: [
@@ -28,6 +28,10 @@ let package = Package(
     .target(
       name: "SpeculidKit",
       dependencies: ["AssetLib", "GampKit", "SwiftDraw"]
+    ),
+    .binaryTarget(
+        name: "CairoSVG",
+        path: "packages/CairoSVG.xcframework"
     ),
     .testTarget(
       name: "SpeculidKitTests",
