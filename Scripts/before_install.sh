ce114f558d6e56eee5a417818d1d3129e8196caa
Leo Dion <leogdion@brightdigit.com>
2020-08-29 21:04:36 -0400

    fixing travis-ci

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -3,6 +3,7 @@
 SWIFT_VER="5.3"
 
 if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
+  echo "No Actions for macOS"
 elif [[ $TRAVIS_OS_NAME = 'linux' ]]; then
   RELEASE_DOT=$(lsb_release -sr)
   RELEASE_NUM=${RELEASE_DOT//[-._]/}
