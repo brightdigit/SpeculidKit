4dde8dc3ad6addcc4a9ea6a56011f074859bf085
Leo Dion <leogdion@brightdigit.com>
2020-08-31 17:07:26 -0400

    added initialize

--- Sources/SpeculidKit.Command/main.swift
+++ Sources/SpeculidKit.Command/main.swift
@@ -1,15 +1,80 @@
 import ArgumentParser
 import Foundation
 import SpeculidKit
+import AssetLib
+
+extension URL {
+    func relativePath(from base: URL) -> String? {
+        // Ensure that both URLs represent files:
+        guard self.isFileURL && base.isFileURL else {
+            return nil
+        }
+
+        // Remove/replace "." and "..", make paths absolute:
+        let destComponents = self.standardized.pathComponents
+        let baseComponents = base.standardized.pathComponents
+
+        // Find number of common path components:
+        var i = 0
+        while i < destComponents.count && i < baseComponents.count
+            && destComponents[i] == baseComponents[i] {
+                i += 1
+        }
+
+        // Build relative path:
+        var relComponents = Array(repeating: "..", count: baseComponents.count - i)
+        relComponents.append(contentsOf: destComponents[i...])
+        return relComponents.joined(separator: "/")
+    }
+}
 
 struct Speculid: ParsableCommand {
   static var configuration = CommandConfiguration(
-    subcommands: [Process.self],
+    subcommands: [Process.self, Initialize.self],
     defaultSubcommand: Process.self
   )
 }
 
 extension Speculid {
+
+  struct Initialize: ParsableCommand {
+    @Option
+    var assetDirectory: String
+
+    @Option
+    var sourceImage: String
+
+    @Argument
+    var destination: String
+
+    func run() throws {
+      let assetDirectoryURL = URL(fileURLWithPath: assetDirectory)
+      let sourceImageFileURL = URL(fileURLWithPath: sourceImage)
+      let destinationSpecFileURL = URL(fileURLWithPath: destination)
+
+      let decoder = JSONDecoder()
+
+      let contentsJSON = assetDirectoryURL.appendingPathComponent("Contents.json")
+      let contentsData = try Data(contentsOf: contentsJSON)
+      let setDocument = try decoder.decode(AssetSpecificationDocument.self, from: contentsData)
+
+     let encoder = JSONEncoder()
+      let outputFormatting: JSONEncoder.OutputFormatting
+      if #available(OSX 10.15, *) {
+        outputFormatting = [.prettyPrinted, .withoutEscapingSlashes]
+      } else {
+        outputFormatting = [.prettyPrinted]
+      }
+      encoder.outputFormatting = outputFormatting
+
+      var file = SpeculidMutableSpecificationsFile(source: SpeculidSpecificationsFile())
+      file.assetDirectoryRelativePath = assetDirectoryURL.relativePath(from: destinationSpecFileURL.deletingLastPathComponent())!
+      file.sourceImageRelativePath = sourceImageFileURL.relativePath(from: destinationSpecFileURL.deletingLastPathComponent())!
+
+      let data = try encoder.encode(SpeculidSpecificationsFile(source: file))
+      try data.write(to: destinationSpecFileURL)
+    }
+  }
   struct Process: ParsableCommand {
     @Argument
     var file: String = ""
